f3b62f9512744d830ee7118a8e1fd182c2c0e477
Bruno Cardoso Ribeiro Pontes <brun.oana@hotmail.com>
2018-02-17 15:41:52 -0300

    Funções básicas de BD adicionadas ao estudo de PDO

--- PDO/exemplo-01.php
+++ PDO/exemplo-01.php
@@ -5,19 +5,33 @@
  * Date: 17/02/2018
  * Time: 14:55
  */
-
+//Conexão
 $conexao = new PDO(
     "mysql:dbname=mysqli_banco;host=localhost",
     "root",
     "");
 
+//Insert =============================================
+$stmt = $conexao->prepare(
+    "INSERT INTO tb_usuario (login, senha)".
+    "values (:login, :senha)");
+$login = "usuario2";
+$senha = "123123123";
+$stmt->bindParam(":login",$login);
+$stmt->bindParam(":senha",$senha);
+//Select =============================================
 $stmt = $conexao->prepare("SELECT * FROM tb_usuario ORDER BY login");
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($results as $row) {
-    foreach ($row as $item => $value) {
-        echo "<strong>".$item.":</strong>".$value."<br/>";
-    }
-    echo "========================================<br/>";
-}
-
+var_dump($results);
+//Update =============================================
+$stmt = $conexao->prepare("UPDATE tb_usuario SET login= :login, senha= :senha where idusuario=:id");
+$id = 1;
+$stmt->bindParam(":login",$login);
+$stmt->bindParam(":senha",$senha);
+$stmt->bindParam(":id",$id);
+$stmt->execute();
+//Delete =============================================
+$stmt = $conexao->prepare("DELETE FROM tb_usuarios WHERE idusuario=:id");
+$stmt->bindParam(":id",$id);
+$stmt->execute();
