4460f704f0fdaae1e485911650330fa1350808bf
Bruno Cardoso Ribeiro Pontes <brun.oana@hotmail.com>
2018-03-05 15:41:09 -0300

    Novos estudos adicionados

--- Cookies/exemplo-01.php
+++ Cookies/exemplo-01.php
@@ -5,3 +5,13 @@
  * Date: 05/03/2018
  * Time: 15:28
  */
+
+$data = array(
+    "nome" => "Bruno Cardoso"
+);
+
+//criando o cookie
+setcookie("NOME_DO_COOKIE", json_encode($data), time() + 3600);
+
+//recuperando cookie
+echo $_COOKIE["NOME_DO_COOKIE"];
